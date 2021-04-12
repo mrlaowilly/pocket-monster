8a0358ce5a88e84f0a5373293a7615a5fc8cd3c5
mrlaowilly <willy.meksavanh@hotmail.fr>
2021-04-12 15:25:22 +0200

    ajout page 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { PokemonsComponent } from './pokemons/list-pokemons/pokemons.component';
+import { PageNotFoundComponent } from './pokemons/page-not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'pokemon/all', pathMatch: 'full'},
   { path: 'pokemon/all', component: PokemonsComponent},
+  { path: '**', component: PageNotFoundComponent}
 ];
 
 // const routes: Routes = [
